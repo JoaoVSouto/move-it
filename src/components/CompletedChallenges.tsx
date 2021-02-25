2ed82e6713d69d34c153054dc9dc4593fd717bee
JoaoVSouto <joaovitorss99@hotmail.com>
2021-02-24 22:55:28 -0300

    Turn completed challenges data dynamic

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,10 +1,21 @@
+import * as React from 'react';
+
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/CompletedChallenges.module.scss';
 
 export function CompletedChallenges() {
+  const { challengesCompleted } = React.useContext(ChallengesContext);
+
+  const challengesCompletedPadded = React.useMemo(
+    () => String(challengesCompleted).padStart(2, '0'),
+    [challengesCompleted]
+  );
+
   return (
     <div className={styles.completedChallengesContainer}>
       <span>Desafios completos</span>
-      <span>00</span>
+      <span>{challengesCompletedPadded}</span>
     </div>
   );
 }
