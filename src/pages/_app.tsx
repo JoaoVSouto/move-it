ad2b638d0cd2a505cac9d6e1f4ea38038e3de139
JoaoVSouto <joaovitorss99@hotmail.com>
2021-02-26 13:33:18 -0300

    Move viewport meta tag to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,20 @@
+import Head from 'next/head';
+
 import { ChallengesProvider } from '../contexts/ChallengesContext';
 
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChallengesProvider>
-      <Component {...pageProps} />
-    </ChallengesProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
+      <ChallengesProvider>
+        <Component {...pageProps} />
+      </ChallengesProvider>
+    </>
   );
 }
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,10 +6,6 @@ export default class MyDocument extends Document {
       <Html lang="pt-BR">
         <Head>
           <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <meta
             name="description"
             content="Move.it | Foque, exercite-se e ganhe níveis!"
           />
