40513c8f6ec25045344652285cf4ac93b840f790
JoaoVSouto <joaovitorss99@hotmail.com>
2021-03-01 18:24:32 -0300

    Populate user data on Profile component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useSession } from 'next-auth/client';
 
 import { ChallengesContext } from '../contexts/ChallengesContext';
 
@@ -7,16 +8,18 @@ import styles from '../styles/components/Profile.module.scss';
 export function Profile() {
   const { level, isLevelUpModalOpen } = React.useContext(ChallengesContext);
 
+  const [session] = useSession();
+
   return (
     <div
       className={`${styles.profileContainer} ${
         isLevelUpModalOpen ? 'blur' : ''
       }`}
     >
-      <img src="https://github.com/JoaoVSouto.png" alt="João Vítor" />
+      <img src={session.user.image} alt={session.user.name} />
 
       <div>
-        <strong>João Vítor</strong>
+        <strong>{session.user.name}</strong>
         <p>
           <img src="icons/level.svg" alt="Nível atual" />
           Level {level}
