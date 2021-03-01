35b656c36b1daba1270b798abf8b28ccf9c8795a
JoaoVSouto <joaovitorss99@hotmail.com>
2021-03-01 18:10:29 -0300

    Change next auth handler to TS

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+
+export default (req: NextApiRequest, res: NextApiResponse) =>
+  NextAuth(req, res, {
+    providers: [
+      Providers.GitHub({
+        clientId: process.env.GITHUB_ID,
+        clientSecret: process.env.GITHUB_SECRET,
+      }),
+    ],
+  });
