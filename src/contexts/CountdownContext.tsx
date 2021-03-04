d41e508f0be230ce395cdeca1ccb21ee23ce10e5
JoaoVSouto <joaovitorss99@hotmail.com>
2021-03-04 18:15:16 -0300

    Update document title with countdown value

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -20,6 +20,10 @@ const INITIAL_COUNTDOWN_SECONDS =
 
 export const CountdownContext = React.createContext({} as CountdownContextData);
 
+const initialDocumentTitle = process.browser
+  ? document.title
+  : 'Início | Move.it';
+
 export function CountdownProvider({ children }: CountdownProviderProps) {
   const { startNewChallenge } = React.useContext(ChallengesContext);
 
@@ -27,12 +31,26 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
   const [isActive, setIsActive] = React.useState(false);
   const [hasFinished, setHasFinished] = React.useState(false);
 
+  const minutes = React.useMemo(() => Math.floor(time / 60), [time]);
+  const seconds = React.useMemo(() => time % 60, [time]);
+
   React.useEffect(() => {
     if (isActive && time > 0) {
       setTimeout(() => {
         // prevents counting an extra second when timer is stopped
         setIsActive(state => {
           if (state) {
+            const minutesPadded = String(
+              seconds - 1 < 0 ? minutes - 1 : minutes
+            ).padStart(2, '0');
+            const secondsPadded = String(
+              seconds - 1 < 0 ? 59 : seconds - 1
+            ).padStart(2, '0');
+
+            const [, documentTitleSuffix] = document.title.split(' | ');
+
+            document.title = `${minutesPadded}:${secondsPadded} | ${documentTitleSuffix}`;
+
             setTime(time - 1);
           }
 
@@ -44,10 +62,11 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
       setIsActive(false);
       startNewChallenge();
     }
-  }, [isActive, time]);
 
-  const minutes = React.useMemo(() => Math.floor(time / 60), [time]);
-  const seconds = React.useMemo(() => time % 60, [time]);
+    if (!isActive) {
+      document.title = initialDocumentTitle;
+    }
+  }, [isActive, time]);
 
   const startCountdown = React.useCallback(() => {
     setIsActive(true);
